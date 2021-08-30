4f10b02998b446fbe86588756aaa49a9d4b2cae1
michalgwozdz <michalgwozdz@payability.com>
2021-08-30 13:18:12 +0200

    michalgwozdz/no-jira/pyb-integration

--- src/utils/pyb-mp.utils.js
+++ src/utils/pyb-mp.utils.js
@@ -1,17 +1,19 @@
-export const PybIntegrationUtils = {
+const PybIntegrationUtils = {
   integrate() {
     return new Promise((res) => setTimeout(() => res({integrated: true}), 1000));
   },
 
-  authorize(supplierKey: string, authKey: string) {
+  authorize(supplierKey, authKey) {
     return new Promise((res) => setTimeout(() => res({authorized: true}), 1000));
   },
 
-  validateSupplierKey(supplierKey: string) {
+  validateSupplierKey(supplierKey) {
     return new Promise((res) => setTimeout(() => res({validation: true}), 500));
   },
 
-  validateAuthKey(authKey: string) {
+  validateAuthKey(authKey) {
     return new Promise((res) => setTimeout(() => res({validation: true}), 500));
   },
 };
+
+module.exports = { PybIntegrationUtils };
