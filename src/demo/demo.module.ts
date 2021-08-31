d7219ccc0daed316d6626fefeaed0df1d0c61c3d
michalgwozdz <michalgwozdz@payability.com>
2021-08-31 18:14:51 +0200

    michalgwozdz/no-jira/isLast-fix

--- src/demo/demo.module.ts
+++ src/demo/demo.module.ts
@@ -1,7 +1,8 @@
-import {NgModule} from '@angular/core';
+import {ApplicationRef, DoBootstrap, Injector, NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DemoComponent} from './demo.component';
 import {PybMpStatusModule} from '../status/pyb-mp-status.module';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,9 @@ import {PybMpStatusModule} from '../status/pyb-mp-status.module';
     CommonModule,
     PybMpStatusModule
   ],
-  bootstrap: [DemoComponent]
+  bootstrap: [environment.production ? [] : DemoComponent]
 })
-export class DemoModule {
+export class DemoModule implements DoBootstrap {
+  ngDoBootstrap(appRef: ApplicationRef): void {
+  }
 }
